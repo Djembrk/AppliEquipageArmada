e55f2609bcb0df5e2ff1e3ee6a6757ea8958497b
Djem <djem.berkpinar76@gmail.com>
2023-02-03 11:55:26 +0100

    finalizing the navigation menu

--- app/src/main/java/com/example/appliequipage/MainActivity.java
+++ app/src/main/java/com/example/appliequipage/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.appliequipage;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+
+    HomeFragment homeFragment = new HomeFragment();
+    CompteurFragment compteurFragment = new CompteurFragment();
+    QRCodeFragment qrCodeFragment = new QRCodeFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,homeFragment).commit();
+                        return true;
+                    case R.id.scanner:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,qrCodeFragment).commit();
+                        return true;
+                    case R.id.ajouter:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,compteurFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
